9613db2aef7a5a0af8c834c74965ab137adce709
redhammer <32373087+dgr1003@users.noreply.github.com>
2022-11-22 20:38:18 +0100

    move speed

--- src/js/app.js
+++ src/js/app.js
@@ -59,7 +59,7 @@ document.addEventListener("DOMContentLoaded", function () {
       },
       "move": {
         "enable": true,
-        "speed": 1,
+        "speed": 8,
         "direction": "none",
         "random": true,
         "straight": false,
